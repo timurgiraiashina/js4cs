84df87d3484f12af2dd9a14307adc4fde00c3643
timurgiraiashina <timurgiraiashina@gmail.com>
2022-06-13 11:03:04 +0300

    8 kolon rastgele sayı fonksiyonu

---
+++ loopDemo.js
@@ -0,0 +1,10 @@
+function sayiUret(ustlimit = 49) {
+    return Math.ceil(Math.random() * ustlimit)
+
+}
+for(i=1;i<=8;i++){
+ console.log(i+".Kolon")
+    for (var j = 1; j <= 6; j++) {
+    console.log(sayiUret())
+}
+}
