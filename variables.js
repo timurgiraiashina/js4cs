fc4c80ed8c24fa68d46c92e845f6709d1a61a3ff
timurgiraiashina <timurgiraiashina@gmail.com>
2022-06-13 11:04:38 +0300

    atanan değişkenlerin türünü yazdıran uygulama

---
+++ variables.js
@@ -0,0 +1,9 @@
+var hamburger="whopper"
+console.log(hamburger)
+console.log(typeof(hamburger))
+hamburger=10
+console.log(hamburger)
+console.log(typeof(hamburger))
+var checksubscribe=false
+console.log(checksubscribe)
+console.log(typeof(checksubscribe))
