7553f0ec1eb45bc3519e8806bfc112b6f4a57580
timurgiraiashina <timurgiraiashina@gmail.com>
2022-06-13 10:01:41 +0300

    if,else if,else yapıları

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+//  sayi=25
+//  if(sayi<50)console.log("sayı 50den küçük")
+not =39
+if(not>50){
+    console.log('Geçtiniz')
+}
+else if(not>=40) {
+    console.log('Bütünlemeye kaldınız')
+}else{console.log("Üzgünüm sınıfta kaldınız")}
